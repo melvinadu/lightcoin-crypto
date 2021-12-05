d6ab3c7f9acb5a4403b8d5f133166b146b023216
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-05 22:51:53 +0000

    created objects to manage banking transactions

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0
+    this.balance = 0;
+  }
 
-  constructor(amount) {
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+}
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount
   }
 
 }
@@ -17,13 +43,14 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('billybob');
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
